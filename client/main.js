13f6c14da3cb7e795de3c1e6da87dbcc3bce9bfc
mariobest250601 <mariobest250601@yahoo.com>
2020-01-30 15:58:41 -0400

    added database

--- client/main.js
+++ client/main.js
@@ -1,22 +1,27 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import 'meteor/jkuester:blaze-bs4'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // this is the default BS theme as example
+import popper from 'popper.js'
+global.Popper = popper // fixes some issues with Popper and Meteor
 import './main.html';
+import '../lib/collection.js';
 
-Template.hello.onCreated(function helloOnCreated() {
+//Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  //this.counter = new ReactiveVar(0);
+//});
+
+Template.myGallery.helpers({
+ allImages(){
+ 	return imagesdb.find();
+ },
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
+//Template.hello.events({
+  //'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+   // instance.counter.set(instance.counter.get() + 1);
+  //},
+//});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import '../lib/collection.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  // var i;
+  if (imagesdb.find().count()<1){
+   	for (i=1;i<26; i++){
+   	  console.log("image"+i);
+   	  imagesdb.insert({
+   		"path":"img_"+i+".jpg",
+   		"title":"title "+i,
+   		"desc":"description "+i
+   	  });
+    }
+  }
+   
 });
