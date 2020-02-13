2ae87edd23bf078a7813533145d8fe5d75d40cdf
mariobest250601 <mariobest250601@yahoo.com>
2020-02-13 15:45:54 -0400

    being able to add images

--- client/main.js
+++ client/main.js
@@ -9,26 +9,52 @@ import './main.html';
 import '../lib/collection.js';
 
 //Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  //this.counter = new ReactiveVar(0);
+	// counter starts at 0
+	//this.counter = new ReactiveVar(0);
 //});
  // increment the counter when button is clicked
-   // instance.counter.set(instance.counter.get() + 1
-  // );
+	 // instance.counter.set(instance.counter.get() + 1
+	// );
 Template.myGallery.helpers({
  allImages(){
- 	return imagesdb.find();
+	return imagesdb.find();
  },
 });
 
 Template.myGallery.events({
-  'click .js-delete'(event, instance)  {
-  	varmyID= this._id;
- 	$("#"+this._id).fadeOut('slow',function(){
- 		imagesdb.remove(myId)});
- 	console.log(myId);
- 	
- 	// console.log("deleting..."+);
- 	// console.log(this._id);
-  },
+	'click .js-delete'(event, instance)  {
+		var myID= this._id;
+	$("#"+this._id).fadeOut('slow',function(){
+		imagesdb.remove({_id:myID});
+		console.log(myID);
+		});
+	},
+
+});	
+
+
+Template.addImage.events({
+	'click .js-Show'(event, instance){
+		console.log("Added Image");
+	},
+	'click .js-close'(event, instance){
+		console.log("closing ");
+	},
+	'click.js-saveImage'(event, instance){
+		// saving the path,title and description	
+		var theTitle= $("#Title").val();
+		var thePath= $ ("#Path").val();
+		var theDesc= $ ("#Desc").val();
+		// console.log("saving Image with title: "+theTitle+"and its path is: "+thePath+"and its description "+theDesc);
+		imagesdb.insert({
+   		"path":thePath,
+   		"title":theTitle,
+   		"desc":theDesc,	
+   	  });
+		// saving and closing modal
+		console.log("saving...");
+		 $('#imgModal').modal('hide');
+	}
+
+
 });
