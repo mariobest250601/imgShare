fffdd575315c6d8c9aef4a1e026ccc86410ed993
mariobest250601 <mariobest250601@yahoo.com>
2020-02-20 15:53:37 -0400

    edit button

--- client/main.js
+++ client/main.js
@@ -30,6 +30,20 @@ Template.myGallery.events({
 		});
 	},
 
+	'click .js-edit'(event, instance){
+		var myID= this._id;
+		$("#editmodal").modal("show");
+		// console.log("lets edit"this._id);
+		var eTitle = imagesdb.findOne({_id:myID}).title;
+		var ePath = imagesdb.findOne({_id:myID}).path;
+		var eDesc = imagesdb.findOne({_id:myID}).desc;
+		$("#editId").val(myID);
+		$("#editTitle").val(eTitle);
+		$("#editPath").val(ePath);
+		$("#editDesc").val(eDesc);
+		$(".editplaceholder").attr("src",ePath);
+		
+	}
 });	
 
 
@@ -40,21 +54,53 @@ Template.addImage.events({
 	'click .js-close'(event, instance){
 		console.log("closing ");
 	},
-	'click.js-saveImage'(event, instance){
+	'click .js-saveImage'(event, instance){
 		// saving the path,title and description	
 		var theTitle= $("#Title").val();
 		var thePath= $ ("#Path").val();
 		var theDesc= $ ("#Desc").val();
 		// console.log("saving Image with title: "+theTitle+"and its path is: "+thePath+"and its description "+theDesc);
 		imagesdb.insert({
-   		"path":thePath,
-   		"title":theTitle,
-   		"desc":theDesc,	
+   		"path": thePath,
+   		"title": theTitle,
+   		"desc": theDesc	
    	  });
 		// saving and closing modal
 		console.log("saving...");
-		 $('#imgModal').modal('hide');
+		  $('#imgModal').modal('hide');
+		  var theTitle= $("#Title").val("");
+		  var thePath= $ ("#Path").val("");
+		  var theDesc= $ ("#Desc").val("");
+	},
+	'input #Path'(event,instance){
+		$(".placeholder").attr("src", $("#Path").val());
+		// console.log("pop "+$("#Path").val());
+	
 	}
+	
 
 
+});
+
+Template.edit.events({
+		
+		'click .js-UpdateImage'(event, instance){
+			var Newtitle= $("#editTitle").val();
+			var NewPath= $("#editPath").val();
+			var NewDesc= $("#editDesc").val();
+			var UpdatedID= $("#editId").val();
+			console.log("updating "+UpdatedID+"title "+Newtitle+"path "+NewPath+"Descrtion"+NewDesc);
+			imagesdb.update({_id: UpdatedID}, 
+				{$set: {
+				"title":Newtitle,
+				"path":NewPath,
+				"desc":NewDesc
+				}}
+			);
+			
+		}
+		
+   		
+   		
+	
 });
